35ac6bc22eb62e3ac856e14694b429910532ff13
Jan Kirenz <jan.kirenz@outlook.de>
2022-04-20 11:24:50 +0200

    Create app.py

---
+++ 1-first-app/app.py
@@ -0,0 +1,37 @@
+# SETUP
+
+import streamlit as st
+import pandas as pd
+
+
+#-------------------
+# CREATE DATA
+
+# Simple dataframe
+df = pd.DataFrame({
+    'A': [1, 4, 3, 2],
+    'B': [10, 20, 30, 40]
+    })
+
+#-------------------
+# START OF APP
+
+#-------------------
+# HEADER
+
+# Title of our app
+
+# Add header
+
+# Add a gif from https://giphy.com/
+
+#-------------------
+# BODY
+
+#-------------------
+# Show static DataFrame
+
+
+#-------------------
+# Bar chart
+
