a70e5471c04b44eff4ca69dd654f091c702a28c8
Jan Kirenz <jan.kirenz@outlook.de>
2022-04-20 13:45:42 +0200

    Initial commit

--- 1-first-app/app.py
+++ 1-first-app/app.py
@@ -2,6 +2,7 @@
 
 import streamlit as st
 import pandas as pd
+import datetime
 
 
 #-------------------
@@ -20,18 +21,44 @@ df = pd.DataFrame({
 # HEADER
 
 # Title of our app
-
+st.title("Meine erste App")
 # Add header
-
+st.header("Mein Header")
 # Add a gif from https://giphy.com/
+#st.markdown("![Katze](https://media.giphy.com/media/ICOgUNjpvO0PC/giphy.gif)")
+
+#st.image('hdm-logo.jpg')
 
 #-------------------
 # BODY
 
+
 #-------------------
 # Show static DataFrame
-
+st.dataframe(df)
 
 #-------------------
 # Bar chart
+st.bar_chart(df)
 
+#-------------------
+
+# SIDEBAR
+
+st.sidebar.radio(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+#-------------------
+
+# WIDGET
+
+age = st.sidebar.slider('How old are you?', 0, 130, 25)
+st.sidebar.write("I'm ", age, 'years old')
+
+
+d = st.sidebar.date_input(
+     "When's your birthday",
+     datetime.date(2019, 7, 6))
+st.write('Your birthday is:', d)
