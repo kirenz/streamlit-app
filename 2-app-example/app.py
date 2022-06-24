9dadcdddba07db66c0113b1951d1261bbb11e6ff
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-06-24 21:20:13 +0200

    Update app.py

--- 2-app-example/app.py
+++ 2-app-example/app.py
@@ -3,64 +3,62 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from pickle import APPEND
-from turtle import width
+import altair as alt
+from pathlib import Path
 
 #-------------------
-# DATA
+#-------------------#
+# IMPORT LOCAL DATA
 
-# Simple dataframe
-df = pd.DataFrame({
-    'A': [1, 4, 3, 2],
-    'B': [10, 20, 30, 40]
-    })
+# Obtain home path
+home_path = str(Path.home())
 
-#-------------------
-# START OF APP
+# Data import (you may need to change the path)
+df = pd.read_csv(home_path + "/streamlit-app/data/oecd.csv")
 
-#-------------------
+# Data preparation
+df = df.sort_values(by=['gdp_per_capita'], ascending=False)
+
+###-------------------###
+# START OF OUR APP
+
+#-------------------#
 # HEADER
 
 # Title of our app
 st.title("Hello World!")
-# Add header
-st.header("This is my first app")
 # Add a gif
 st.markdown("![Alt Text](https://media.giphy.com/media/MeJgB3yMMwIaHmKD4z/giphy.gif)")
 
-#-------------------
+#-------------------#
 # BODY
 
-#-------------------
-# Show static DataFrame
-st.subheader("Show Dataframe")
-st.write("Here's our first attempt at using data to create a table:")
-st.write(df)
-
-#-------------------
-# Highlight some attributes
+# Show df ans highlight some attributes
 st.write("DataFrame with highlight_max:")
 st.dataframe(df.style.highlight_max(axis=0))
 
-#-------------------
+
 # Bar chart
 st.subheader("Bar chart")
-# standard bar chart
-st.bar_chart(df)
 
-#-------------------
+c = alt.Chart(df).mark_bar().encode(
+    x='country',
+    y='life_satisfaction',
+    tooltip = ['country', "life_satisfaction"]
+)
+
+st.altair_chart(c, use_container_width=True)
+
 # Show metric
 st.subheader("Display Metrics")
 st.metric("My metrics", 32, 4)
 
-#-------------------
 # Add slider with user input
 st.subheader("Slider")
 st.write("This is a Slider")
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
 
-#-------------------
 # Show code
 st.subheader("Show some code")
 
