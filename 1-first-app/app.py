dafe3d07f1ab74d545405c1fd7d79fbc4ff539ae
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-06-24 20:54:52 +0200

    Update app.py

--- 1-first-app/app.py
+++ 1-first-app/app.py
@@ -5,7 +5,6 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-import datetime
 from pathlib import Path
 
 #-------------------#
@@ -13,11 +12,13 @@ from pathlib import Path
 
 # Obtain home path
 home_path = str(Path.home())
-# Data import
+
+# Data import (you may need to change the path)
 df = pd.read_csv(home_path + "/streamlit-app/data/oecd.csv")
 
+
 ###-------------------###
-# START OF APP
+# START OF OUR APP
 
 #-------------------#
 # HEADER
@@ -25,35 +26,12 @@ df = pd.read_csv(home_path + "/streamlit-app/data/oecd.csv")
 # Title of our app
 st.title("My first App")
 
-# Image
+# Add image
 st.image('hdm-logo.jpg')
 
 # Add header
 st.header("My header")
 
-#-------------------#
-# BODY
-
-#-------------------
-# Show static DataFrame
-
-st.write("Take a look at my data")
-
-st.dataframe(df)
-
-#-------------------
-# Make a chart with altair
-
-st.write("Take a look at my chart")
-
-c = alt.Chart(df).mark_circle().encode(
-     x='life_satisfaction', 
-     y='gdp_per_capita', 
-     color='country',
-     tooltip = ['life_satisfaction', 'gdp_per_capita', 'country']
-     )
-
-st.altair_chart(c, use_container_width=True)
 
 #-------------------#
 # SIDEBAR
@@ -61,17 +39,31 @@ st.altair_chart(c, use_container_width=True)
 # Header
 st.sidebar.header("This is my sidebar")
 
-# Slider
+# Make a slider
 satisfaction = st.sidebar.slider('What is your life satisfaction?', 0, 10, 1)
 
+# Show output of slider selection
 st.sidebar.write("My life satisfaction is around ", satisfaction, 'points')
 
-# Date
-d = st.sidebar.date_input(
-     "When's your birthday",
-     datetime.date(2019, 7, 6))
 
-st.sidebar.write('Your birthday is:', d)
+#-------------------#
+# BODY
+
+st.write("Take a look at my data")
+# Show static DataFrame
+st.dataframe(df)
+
+st.write("Take a look at my chart")
+# Make a chart with altair
+c = alt.Chart(df).mark_circle().encode(
+     x='life_satisfaction', 
+     y='gdp_per_capita', 
+     color='country'
+     )
+
+# Show plot
+st.altair_chart(c, use_container_width=True)
+
 
 ###-------------------###
 # END OF APP
