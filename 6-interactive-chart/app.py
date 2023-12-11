0bece3bf781e2ac05be19740fefccf07afd8cf21
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2023-12-11 10:54:26 +0100

    Update app.py

--- 6-interactive-chart/app.py
+++ 6-interactive-chart/app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from vega_datasets import data
 
 # -------------------#
 # IMPORT DATA
@@ -59,30 +60,128 @@ c = alt.Chart(filtered_df).mark_circle().encode(
 st.altair_chart(c, use_container_width=True)
 
 ### -------------------###
+st.title("Interactive Rectangular Brush Plot")
 
-st.write("Take a look at my interactive chart")
+
+st.markdown("You can use *Markdown* to **explain** the ***content***.")
+st.markdown('''
+    :red[Example] :orange[shows] :green[how] :blue[to] :violet[add]
+    :gray[a simple] :rainbow[rectangular brush to a scatter plot].''')
+
+
+multi = '''By clicking and dragging on the plot,  
+you can highlight points within the range.
+
+'''
+st.markdown(multi)
+
+
 # Make a chart with altair
-highlight = alt.selection_point(on='mouseover', fields=['symbol'], nearest=True)
+brush = alt.selection_interval()
 
-base = alt.Chart(source).encode(
+c2 = alt.Chart(df).mark_point().encode(
     x='life_satisfaction',
     y='gdp_per_capita',
-    color='country'
+    color=alt.condition(brush, 'country', alt.value('grey')),
+).add_params(brush)
+
+# Show plot
+st.altair_chart(c2, use_container_width=True)
+
+
+st.title("Streamlit emoji")
+
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+
+st.markdown("[Streamlit emoji shortcodes](https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app)")
+
+
+### -------------------###
+
+st.title('Multiple Interactions')
+
+
+movies = alt.UrlData(
+    data.movies.url,
+    format=alt.DataFormat(parse={"Release_Date":"date"})
+)
+ratings = ['G', 'NC-17', 'PG', 'PG-13', 'R']
+genres = [
+    'Action', 'Adventure', 'Black Comedy', 'Comedy',
+    'Concert/Performance', 'Documentary', 'Drama', 'Horror', 'Musical',
+    'Romantic Comedy', 'Thriller/Suspense', 'Western'
+]
+
+base = alt.Chart(movies, width=200, height=200).mark_point(filled=True).transform_calculate(
+    Rounded_IMDB_Rating = "floor(datum.IMDB_Rating)",
+    Hundred_Million_Production =  "datum.Production_Budget > 100000000.0 ? 100 : 10",
+    Release_Year = "year(datum.Release_Date)"
+).transform_filter(
+    alt.datum.IMDB_Rating > 0
+).transform_filter(
+    alt.FieldOneOfPredicate(field='MPAA_Rating', oneOf=ratings)
+).encode(
+    x=alt.X('Worldwide_Gross:Q').scale(domain=(100000,10**9), clamp=True),
+    y='IMDB_Rating:Q',
+    tooltip="Title:N"
 )
 
-points = base.mark_circle().encode(
-    opacity=alt.value(0)
-).add_params(
-    highlight
-).properties(
-    width=600
+# A slider filter
+year_slider = alt.binding_range(min=1969, max=2018, step=1, name="Release Year")
+slider_selection = alt.selection_point(bind=year_slider, fields=['Release_Year'])
+
+filter_year = base.add_params(
+    slider_selection
+).transform_filter(
+    slider_selection
+).properties(title="Slider Filtering")
+
+# A dropdown filter
+genre_dropdown = alt.binding_select(options=genres, name="Genre")
+genre_select = alt.selection_point(fields=['Major_Genre'], bind=genre_dropdown)
+
+filter_genres = base.add_params(
+    genre_select
+).transform_filter(
+    genre_select
+).properties(title="Dropdown Filtering")
+
+#color changing marks
+rating_radio = alt.binding_radio(options=ratings, name="Rating")
+rating_select = alt.selection_point(fields=['MPAA_Rating'], bind=rating_radio)
+
+rating_color_condition = alt.condition(
+    rating_select,
+    alt.Color('MPAA_Rating:N').legend(None),
+    alt.value('lightgray')
 )
 
-lines = base.mark_line().encode(
-    size=alt.condition(~highlight, alt.value(1), alt.value(3))
+highlight_ratings = base.add_params(
+    rating_select
+).encode(
+    color=rating_color_condition
+).properties(title="Radio Button Highlighting")
+
+# Boolean selection for format changes
+input_checkbox = alt.binding_checkbox(name="Big Budget Films ")
+checkbox_selection = alt.param(bind=input_checkbox)
+
+size_checkbox_condition = alt.condition(
+    checkbox_selection,
+    alt.Size('Hundred_Million_Production:Q'),
+    alt.SizeValue(25)
 )
 
-points + lines
+budget_sizing = base.add_params(
+    checkbox_selection
+).encode(
+    size=size_checkbox_condition
+).properties(title="Checkbox Formatting")
+
+(filter_year | filter_genres) & (highlight_ratings | budget_sizing)
+
 
 
 
