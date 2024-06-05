d75abdd7ee9cb471a8b62dd75c53ce9b1a6e203b
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2024-06-05 10:09:11 +0200

    Update app.py

--- 4-multi-page/app.py
+++ 4-multi-page/app.py
@@ -1,31 +1,31 @@
- # Importing required packages
+# Importing the Streamlit package which is used to create web applications
 import streamlit as st
-import pandas as pd
-import altair as alt
 
+# Importing custom modules for different pages of the application
 import page_one
 import page_two
-from my_data import df
-from streamlit_chart import c
+import page_three
+import page_four
 
 
-st.title('Multi-Page App')
-
+# Defining a dictionary that maps page names to their respective modules
 PAGES = {
-    "Page one": page_one,
-    "Page two": page_two
-    }
+    "User Persona": page_one,   # Key-value pair for the first page
+    "Unternehmensdaten": page_two,   # Key-value pair for the second page
+    "Kennzahlen": page_three, # Key-value pair for the third page
+    "Datenübersicht": page_four, # Key-value pair for the fourth page
 
+}
 
-
+# Creating a sidebar in the web application for navigation
 st.sidebar.title('Navigation')
+
+# Adding a radio button widget to the sidebar for selecting a page
+# The keys of the PAGES dictionary are used as the options
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
+# Getting the module of the selected page
 page = PAGES[selection]
 
-st.dataframe(df)
-
-st.altair_chart(c, use_container_width=True)
-
-
+# Calling the 'app' function from the selected page's module to display the page content
 page.app()
